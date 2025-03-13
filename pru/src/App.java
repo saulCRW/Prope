03a088cd9f689bcd52ef7e23957cc9c40251cd6f
Saul Contreras <saul2005@gmail.com>
2025-03-12 22:04:44 -0600

    suma numeros

--- pru/src/App.java
+++ pru/src/App.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
         System.out.println("input 2 numbers to add");
-        int a = 10;
-        int b = 20;     
+        int a =s.nextInt();
+        int b =s.nextInt();
+        
         System.out.println("Sum of a and b is: " + (a + b));
 
     }
