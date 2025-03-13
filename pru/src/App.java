a05b5c339d548d8ebf5f3858563164f8f343e4d5
Saul Contreras <saul2005@gmail.com>
2025-03-12 22:01:36 -0600

    suma

---
+++ pru/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("input 2 numbers to add");
+        int a = 10;
+        int b = 20;     
+        System.out.println("Sum of a and b is: " + (a + b));
+
+    }
+}
